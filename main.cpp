489e46ee0307ee215ac1ae1d3028a70cc16e4a3a
crybug123 <s11333@kcis.com.tw>
2022-07-28 20:12:07 +0800

    7/28

--- main.cpp
+++ main.cpp
@@ -8,112 +8,43 @@
  * the standard input according to the problem statement.
  **/
 
-void countForX(const char GAME[], int cur_index, int &ABC)
-{
-    ABC+=10;
-    if(GAME[cur_index+2]=='X'){
-        ABC+=10;
-        if(GAME[cur_index+4]=='X'){
-            ABC+=10;
-        }
-        else if(GAME[cur_index+4]=='-'){
-            ABC+=0;
-        }
-        else{
-            ABC+=GAME[cur_index+4];
-        }
+void countForALL(const char GAME[], int cur_index, int &ABC, int &n){
+    int counter = 0;
+    while(GAME[cur_index+1]==' '){
+        cur_index++;
+        n++;
     }
-    else if(GAME[cur_index+2]=='-'){
-        if(GAME[cur_index+3]=='/'){
+    while (GAME[cur_index+1]!=' '){
+        counter = 0;
+        if (GAME[cur_index]=='X'){
             ABC+=10;
+            while (counter!=2){
+                ABC+=GAME[cur_index+1];
+                counter++;
+            }
         }
-        else{
-            ABC+=GAME[cur_index+3];
-        }
-    }
-    else{
-        ABC+=GAME[cur_index+2];
-        if(GAME[cur_index+3]=='-'){
-            ABC+=0;
-        }
-        else if(GAME[cur_index+3]=='/'){
-            ABC+=(10-(GAME[cur_index+3]));
-        }
-        else{
-            ABC+=GAME[cur_index+3];
-        }
-    }
-}
-
-void countforN9X(const char GAME[], int cur_index, int &ABC)
-{
-        if (GAME[cur_index+1]=='X'){
-            ABC+=10;
-        }
-        else if(GAME[cur_index+1]=='-'){
-            ABC+=0;
-        }
-        else if(GAME[cur_index+1]=='/'){
-            ABC+=(10-(GAME[cur_index]-'0'));
-        }
-        else if(GAME[cur_index+1]!=' '){
-            ABC+=GAME[cur_index+1]-'0';
-        }
-        if (GAME[cur_index+2]=='X'){
-            ABC+=10;
-        }
-        else if (GAME[cur_index+2]=='-'){
-            ABC+=0;
-        }
-        else if(GAME[cur_index+2]=='/'){
-            if (GAME[cur_index+1]=='-'){
+        else if (GAME[cur_index]=='/'){
+            if (GAME[cur_index-1]=='-'){
                 ABC+=10;
             }
             else{
-                ABC+=(10-(GAME[cur_index+1]-'0'));
+                ABC+=(10-(GAME[cur_index-1]-'0'));
+            }
+            while (counter==1){
+                ABC+=GAME[cur_index+1];
+                counter++;
             }
         }
-        else if(GAME[cur_index+2]!=' '){
-            ABC+=GAME[cur_index+2]-'0';
+        else if (GAME[cur_index]=='-'){
+            ABC+=0;
         }
-}
-
-void countforSlash(const char GAME[], int cur_index, int &ABC)
-{
-    if(GAME[cur_index-1]=='-'){
-        cur_index+=1;
-        ABC+=0;
-    }
-    else if(GAME[cur_index-1]!='-'){
-        ABC+=(10-(GAME[cur_index-1]-'0'));
-    }
-    if(GAME[cur_index+2]=='X'){
-        ABC+=10;
-    }
-    else if(GAME[cur_index+2]=='-'){
-        ABC+=0;
-    }
-    else{
-        ABC+=GAME[cur_index+2];
+        else{
+            ABC+=GAME[cur_index];
+        }
+        break;   
     }
 }
 
-void countforN9Slash(const char GAME[], int cur_index, int &ABC){
-    if (GAME[cur_index+1]=='-'){
-        ABC+=0;
-    }
-    else if(GAME[cur_index+1]=='X'){
-        ABC+=10;
-    }
-    else{
-        ABC+=GAME[cur_index+1]-'0';
-    }
-}
-
-int countforNum(const char GAME[], int cur_index, int &ABC)
-{
-    ABC+=GAME[cur_index];
-}
 int main()
 {
     int N;
@@ -134,26 +65,15 @@ int main()
                 // fprintf(stderr, "Debug messages...%c %d\n",GAME[i],i);
                 if (GAME[i] == 'X')
                 {
-                    if (n == 9 && c==0){
-                        countforN9X(GAME, i, ABC[n]);
+                    countForALL(GAME, i, ABC[n], n);
+                    if (n == 9)
                         break;
-                    }
-                    else if(n == 9){
-                        c+=1;
-                    }
-                    else{
-                        countForX(GAME, i, ABC[n]);
-                    }
                 }
                 else if (GAME[i] == '/')
                 {
-                    if (n == 9){
-                        countforN9Slash(GAME, i, ABC[n]);
+                    countForALL(GAME, i, ABC[n], n);
+                    if (n == 9)
                         break;
-                    }
-                    else{
-                        countforSlash(GAME, i, ABC[n]);
-                    }
                 }
                 else if (GAME[i] == ' ')
                 {
